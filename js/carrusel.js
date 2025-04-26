1a0d9cdadd916256b295db5b489b6169bfb5df96
AnduellgDeveloper <anduellgames@gmail.com>
2025-04-26 11:19:19 -0500

    Cambios

--- js/carrusel.js
+++ js/carrusel.js
@@ -32,4 +32,4 @@
         // Configuración del autoscroll cada 5 segundos
         setInterval(() => {
             nextButton.click();
-        }, 2900); // Cambia de imagen cada 5 segundos
+        }, 3200); // Cambia de imagen cada 5 segundos
